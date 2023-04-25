06f359ff79208f8e3ceec11b9e96d4de25a05b20
OkkyAdnsyh <landing.pageportofolio@gmail.com>
2023-04-26 00:14:17 +0700

    setup chat components

--- components/Elements/Chats/Bubble/Bubble.tsx
+++ components/Elements/Chats/Bubble/Bubble.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Container from '@/components/Blocks/Container/Container';
+import Paragraf from '@/components/Blocks/Paragraf/Paragraf';
+import Styles from './Bubble.module.scss';
+import { NextPage } from 'next';
+import { IDataChatProps } from '@/utils/Interfaces/Interfaces';
 
-const Bubble = () => {
+const Bubble : NextPage<IDataChatProps>= (props) => {
+
+  const {userMessage, messageTime} = props
+
   return (
-    <div>Bubble</div>
+    <>
+      <Container className={Styles.container}>
+        <Paragraf className={Styles.message}>
+          {userMessage}
+        </Paragraf>
+        <Paragraf className={Styles.time}>
+          {messageTime}
+        </Paragraf>
+      </Container>
+    </>
   )
 }
 
